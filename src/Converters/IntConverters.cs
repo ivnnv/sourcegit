da2e85bfdbbc14dd1b880396685729536ba26e79
Ivn Nv <ivnnv@hotmail.com>
2026-08-18 10:13:38 +0300

    chore: drop BookmarkBrushConverter and IsNegativeOne, replaced by the Bookmark controls [fork:colored-tabs]

--- src/Converters/IntConverters.cs
+++ src/Converters/IntConverters.cs
@@ -1,30 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Globalization;
-
-using Avalonia;
-using Avalonia.Controls;
+﻿using Avalonia;
 using Avalonia.Data.Converters;
 
 namespace SourceGit.Converters
 {
-    // [fork:colored-tabs] Brush converter for custom bookmark colors
-    public class BookmarkBrushConverter : IMultiValueConverter
-    {
-        public static readonly BookmarkBrushConverter Instance = new();
-
-        public object Convert(IList<object> values, Type targetType, object parameter, CultureInfo culture)
-        {
-            var bookmark = values.Count > 0 && values[0] is int b ? b : 0;
-            var bookmarkCustom = values.Count > 1 && values[1] is uint bc ? bc : 0u;
-
-            if (bookmark == -1 && bookmarkCustom != 0)
-                return new SolidColorBrush(Color.FromUInt32(bookmarkCustom));
-
-            return Models.Bookmarks.Get(bookmark) ?? App.Current?.FindResource("Brush.FG1") as IBrush;
-        }
-    }
-
     public static class IntConverters
     {
         public static readonly FuncValueConverter<int, bool> IsGreaterThanZero =
@@ -38,10 +16,6 @@ namespace SourceGit.Converters
 
         public static readonly FuncValueConverter<int, bool> IsNotOne =
             new(v => v != 1);
-
-        // [fork:colored-tabs] Used in EditRepositoryNode to mark the "Custom..." dropdown item (sentinel = -1)
-        public static readonly FuncValueConverter<int, bool> IsNegativeOne =
-            new(v => v == -1);
 
         public static readonly FuncValueConverter<int, Thickness> ToTreeMargin =
             new(v => new Thickness(v * 16, 0, 0, 0));
